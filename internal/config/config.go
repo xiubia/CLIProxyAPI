03dd605fbbb7b5a3a8375618907dfef592e2038e
xiubia <1473144550@qq.com>
2026-05-03 17:08:24 +0800

    Use xiubia management panel repository by default

--- internal/config/config.go
+++ internal/config/config.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	DefaultPanelGitHubRepository = "https://github.com/router-for-me/Cli-Proxy-API-Management-Center"
+	DefaultPanelGitHubRepository = "https://github.com/xiubia/Cli-Proxy-API-Management-Center"
 	DefaultPprofAddr             = "127.0.0.1:8316"
 )
 
